7bf55f69d49448554d343a3ef680908f66a8455a
Felype Nascimento <Felype.nasc@hotmail.com>
2022-02-17 16:49:46 -0300

    ranking ajax ok

--- frontend/src/initialPage/script.js
+++ frontend/src/initialPage/script.js
@@ -1,3 +1,22 @@
-$(document).ready(
-    
-)
+const apiURL = "http://localhost:3000/";
+
+$(document).ready(function () {
+    $.ajax({
+        url: apiURL + `ranking/rankingData?value=ranking`,
+    }).done((data) => {
+        console.log(data);
+        renderRanking(data)
+    });
+});
+
+function renderRanking(data) {
+    data.forEach((element) => {
+        $("#highscores-tbody").append(`
+            <tr>
+                <td>lvl${element.lvl}</td>
+                <td>${element.nome}</td>
+                <td>${element.score}</td>
+            </tr>
+        `);
+    });
+}
