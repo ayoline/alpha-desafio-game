5e2344978929d02096c4906a9d323b046f8daffd
Mark <marcosrafaelp.camargo@gmail.com>
2022-02-17 17:32:54 -0300

    v2.1-front

--- frontend/src/script.js
+++ frontend/src/script.js
@@ -1,1 +1,22 @@
 const apiURL = "http://localhost:3000/";
+
+$(document).ready(function () {
+    $.ajax({
+        url: apiURL + `ranking/rankingData?value=ranking`,
+    }).done((data) => {
+        console.log(data);
+        renderRanking(data)
+    });
+});
+
+function renderRanking(data) {
+    data.forEach((element) => {
+        $("#highscores-tbody").append(`
+            <tr>
+                <td>lvl${element.lvl}</td>
+                <td>${element.nome}</td>
+                <td>${element.score}</td>
+            </tr>
+        `);
+    });
+}
