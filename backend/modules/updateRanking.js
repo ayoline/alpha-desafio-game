bc7f1d39b2450a7a7ebd8c8d74a4e4fe90a01cf7
Felype Nascimento <Felype.nasc@hotmail.com>
2022-02-20 15:42:12 -0300

    updateRanking module ok

--- backend/modules/updateRanking.js
+++ backend/modules/updateRanking.js
@@ -1,36 +1,31 @@
-const fs = require("fs");
+const fs = require('fs');
 
-function updateRanking(playerName, playerScore, playerLvl) {
-    const orderedRanking = JSON.parse(
-        fs.readFileSync("data/ranking.json", "utf8")
-    );
+function updateRanking(_id, _lvl, _score) {
+    const rankingToBeOrdered = JSON.parse(fs.readFileSync('data/ranking.json', 'utf8'));
+    const playerData = {};
+    playerData.id = _id;
+    playerData.lvl = _lvl;
+    playerData.score = _score;
 
-    orderedRanking.push({
-        "player": playerName,
-        "lvl": playerLvl,
-        "score": playerScore
-    });
-    orderedRanking.sort((a, b) => (Number(a.score) > Number(b.score) ? 1 : -1));
-    orderedRanking.reverse();
-    removeLowerPositions(orderedRanking);
+    if (playerData.id && playerData.lvl && playerData.score) {
+        rankingToBeOrdered.push(playerData);
+        rankingToBeOrdered.sort((a, b) => Number(a.score) > Number(b.score) ? 1 : -1);
+        rankingToBeOrdered.reverse();
+        removeLowerPositions(rankingToBeOrdered);
 
-    fs.writeFile(
-        "data/ranking.json",
-        JSON.stringify(orderedRanking),
-        function (err) {
+        fs.writeFile('data/ranking.json', JSON.stringify(rankingToBeOrdered), function (err) {
             if (!err) {
-                return orderedRanking;
+                return rankingToBeOrdered;
             } else {
-                console.log("Error: " + err);
                 return err;
             }
-        }
-    );
+        });
+    }
+}
 
-    function removeLowerPositions(_array) {
-        while (_array.length > 10) {
-            _array.pop();
-        }
+function removeLowerPositions(_array) {
+    while (_array.length > 10) {
+        _array.pop();
     }
 }
 
