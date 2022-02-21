98b74fed1b14a7ea9a39cec34248cfcd966d4758
Marcos Peter <mpeterlobato@gmail.com>
2022-02-21 18:19:58 -0300

    Fixing the 'getResult' function

--- backend/modules/generateOperations.js
+++ backend/modules/generateOperations.js
@@ -85,25 +85,22 @@ function generateOperations(_qtt, _lvl) {
     'thirdOperator': []
   };
 
+  const response = [];
+
 	let level = {};
 
 	//Populating the entries array
 	while(entries.length < qtt) {
-    // console.log('entrou no problema');
 		entries.push(validateEntry(entries, 0));
 	};
 	// console.log(entries);
 
 	//Populating the operators array
 	populateOperators(difficultyByLevel[lvl].qtt);
-  
-	// console.log(args.firstOperator);
-	// console.log(args.secondOperator);
-	// console.log(args.thirdOperator);
-  // console.log(args);
 
 	//Populating the results array
-	getResult(args/*firstOperator,firstOperands,secondOperator,secondOperands,thirdOperator*/);
+  getResultByOperator(args);
+	// getResult(args);
 
 	//Populating the operations array
 	getFinalOperations(difficultyByLevel[lvl].qtt, difficultyByLevel[lvl].questions);
@@ -113,23 +110,27 @@ function generateOperations(_qtt, _lvl) {
 		"operations": operations
 	};
 
+
+  response.push(entries);
+  response.push(results);
+
+
+
+
+
 	//Function to get the complete math operations
 	function getFinalOperations(_qtt, _qts) {
 		const qtt = _qtt;
 		const qts = _qts;
 
 		if(qtt === 2) {
-			for(let i = 0; i < qts; i++) {
-				operations.push(
-					`q0${i+1}: ${args.firstOperator[i]} ${args.firstOperands[i]} ${args.secondOperator[i]} = ${results[i]}`
-				);
-			};
+			operations.push(
+        `q1: ${args.firstOperator[0]} ${args.firstOperands[0]} ${args.secondOperator[0]} = ${results[0]}`
+      );
 		} else if(qtt === 3) {
-			for(let i = 0; i < qtt; i++) {
-				operations.push(
-					`q0${i+1}: ${args.firstOperator[i]} ${args.firstOperands[i]} ${args.secondOperator[i]} ${args.secondOperands[i]} ${args.thirdOperator[i]} = ${results[i]}`
-				);
-			};
+			operations.push(
+        `q1: ${args.firstOperator[0]} ${args.firstOperands[0]} ${args.secondOperator[0]} ${args.secondOperands[0]} ${args.thirdOperator[0]} = ${results[0]}`
+      );
 		};
 	};
 
@@ -138,94 +139,132 @@ function generateOperations(_qtt, _lvl) {
 		const qtt = _qtt;
 
 		if(qtt === 2) {
-			while(args.firstOperator.length < 3 && args.secondOperator.length < 3) {
-				args.firstOperator.push(validateEntry(args.firstOperator, 1));
-				args.secondOperator.push(validateEntry(args.secondOperator, 1));
-				args.firstOperands.push(operands[rand(operands.length)]);
-			};
+			args.firstOperator.push(validateEntry(args.firstOperator, 1));
+			args.secondOperator.push(validateEntry(args.secondOperator, 1));
+			args.firstOperands.push(operands[rand(operands.length)]);
 		} else if(qtt === 3) {
-			while(args.firstOperator.length < 3 && args.secondOperator.length < 3) {
-				args.firstOperator.push(validateEntry(args.firstOperator, 1));
-				args.secondOperator.push(validateEntry(args.secondOperator, 1));
-				args.thirdOperator.push(validateEntry(args.thirdOperator, 1));
-				args.firstOperands.push(difficultyByLevel[lvl].op[0]);
-				args.secondOperands.push(difficultyByLevel[lvl].op[1] || difficultyByLevel[lvl].op[0]);
-			};
+			args.firstOperator.push(validateEntry(args.firstOperator, 1));
+			args.secondOperator.push(validateEntry(args.secondOperator, 1));
+			args.thirdOperator.push(validateEntry(args.thirdOperator, 1));
+			args.firstOperands.push(difficultyByLevel[lvl].op[0]);
+			args.secondOperands.push(difficultyByLevel[lvl].op[1] || difficultyByLevel[lvl].op[0]);
 		};
 	};
 
-	//Function to get the result of the operation
-	function getResult(_obj) {
+  // Testing the new function to get the result, still need to test and handle with the division problems
+  // The function is working fine
+  // Just working on the division problems now
+  function getResultByOperator(_obj) {
     const obj = _obj;
-		const fO = obj.firstOperator;
-		const fOp = obj.firstOperands;
-		const sO = obj.secondOperator;
-		const sOp = obj.secondOperands;
-		const tO = obj.thirdOperator;
+    const fO = obj.firstOperator;
+    const fOp = obj.firstOperands;
+    const sO = obj.secondOperator;
+    const sOp = obj.secondOperands;
+    const tO = obj.thirdOperator;
 
-		let tmpRes;
-		let res;
+    let tmpRes;
+    let res;
 
-    // console.log(sOp.length);
+    if(fOp[0] === '+') {
+      tmpRes = +(fO[0] + sO[0]);
+    } else if(fOp[0] === '-') {
+      tmpRes = +(fO[0] - sO[0]);
+    } else if (fOp[0] === '*') {
+      tmpRes = +(fO[0] * sO[0]);
+    } else if(fOp[0] === '/') {
+      tmpRes = +(Math.floor(fO[0] / sO[0]));
+    };
 
-		if(sOp.length > 0) {
-			for(let i = 0; i <= fO.length; i++) {
-				if(fOp[i] === '+') {
-					tmpRes = +(fO[i] + sO[i]);
-					if(sOp[i] === '+') {
-						res = +(tmpRes + tO[i]);
-					};
-					results.push(res);
-					tmpRes = '';
-					res = '';
-				} else if(fOp[i] === '-') {
-					tmpRes = +(fO[i] - sO[i]);
-					if(sOp[i] === '-') {
-						res = +(tmpRes - tO[i]);
-					};
-					results.push(res);
-					tmpRes = '';
-					res = '';
-				} else if (fOp[i] === '*') {
-					tmpRes = +(fO[i] * sO[i]);
-					if(sOp[i] === '*') {
-						res = +(tmpRes * tO[i]);
-					};
-					results.push(res);
-					tmpRes = '';
-					res = '';
-				} else if(fOp[i] === '/') {
-					tmpRes = +(Math.floor(fO[i] / sO[i]));
-					if(sOp[i] === '/') {
-						res = +(Math.floor(tmpRes / tO[i]));
-					};
-					results.push(res);
-					tmpRes = '';
-					res = '';
-				};
-			};
-		} else {
-			for(let i = 0; i <= fO.length; i++) {
-				if(fOp[i] === '+') {
-					res = +(fO[i] + sO[i]);
-					results.push(res);
-					res = '';
-				} else if(fOp[i] === '-') {
-					res = +(fO[i] - sO[i]);
-					results.push(res);
-					res = '';
-				} else if (fOp[i] === '*') {
-					res = +(fO[i] * sO[i]);
-					results.push(res);
-					res = '';
-				} else if(fOp[i] === '/') {
-					res = +(Math.floor(fO[i] / sO[i]));
-					results.push(res);
-					res = '';
-				};
-			};
-		}
-	};
+    if(sOp.length > 0) {
+      if(sOp[0] === '+') {
+        res = +(tmpRes + tO[0]);
+      } else if(sOp[0] === '-') {
+        res = +(tmpRes - tO[0]);
+      } else if (sOp[0] === '*') {
+        res = +(tmpRes * tO[0]);
+      } else if(sOp[0] === '/') {
+        res = +(tmpRes / tO[0]);
+      };
+
+      results.push(res);
+      return res;
+
+    } else {
+      results.push(tmpRes);
+      return tmpRes;
+    };      
+  };
+
+
+
+
+	//Function to get the result of the operation
+  //Needs to handle the order of the divisions (ensure that the first operator is greater than the second)
+	// function getResult(_obj) {
+  //   const obj = _obj;
+	// 	const fO = obj.firstOperator;
+	// 	const fOp = obj.firstOperands;
+	// 	const sO = obj.secondOperator;
+	// 	const sOp = obj.secondOperands;
+	// 	const tO = obj.thirdOperator;
+
+	// 	let tmpRes;
+	// 	let res;
+
+	// 	if(sOp.length > 0) {
+	// 		if(fOp[0] === '+') {
+  //       tmpRes = +(fO[0] + sO[0]);
+  //       if(sOp[0] === '+') {
+  //         res = +(tmpRes + tO[0]);
+  //       };
+  //       results.push(res);
+  //       tmpRes = '';
+  //       res = '';
+  //     } else if(fOp[0] === '-') {
+  //       tmpRes = +(fO[0] - sO[0]);
+  //       if(sOp[0] === '-') {
+  //         res = +(tmpRes - tO[0]);
+  //       };
+  //       results.push(res);
+  //       tmpRes = '';
+  //       res = '';
+  //     } else if (fOp[0] === '*') {
+  //       tmpRes = +(fO[0] * sO[0]);
+  //       if(sOp[0] === '*') {
+  //         res = +(tmpRes * tO[0]);
+  //       };
+  //       results.push(res);
+  //       tmpRes = '';
+  //       res = '';
+  //     } else if(fOp[0] === '/') {
+  //       tmpRes = +(Math.floor(fO[0] / sO[0]));
+  //       if(sOp[0] === '/') {
+  //         res = +(Math.floor(tmpRes / tO[0]));
+  //       };
+  //       results.push(res);
+  //       tmpRes = '';
+  //       res = '';
+  //     };
+	// 	} else {
+	// 		if(fOp[0] === '+') {
+  //       res = +(fO[0] + sO[0]);
+  //       results.push(res);
+  //       res = '';
+  //     } else if(fOp[0] === '-') {
+  //       res = +(fO[0] - sO[0]);
+  //       results.push(res);
+  //       res = '';
+  //     } else if (fOp[0] === '*') {
+  //       res = +(fO[0] * sO[0]);
+  //       results.push(res);
+  //       res = '';
+  //     } else if(fOp[0] === '/') {
+  //       res = +(Math.floor(fO[0] / sO[0]));
+  //       results.push(res);
+  //       res = '';
+  //     };
+	// 	}
+	// };
 
 	//Function to generate a random number
 	function rand(_min = 0, _max) {
@@ -247,8 +286,6 @@ function generateOperations(_qtt, _lvl) {
 
 		let value = (type === 0) ? Math.floor(Math.random() * (max - min) + min) : entries[rand(entries.length)];
 
-    // console.log(value);
-
 		if(type === 0) {
 			while(arr.includes(value)) {
 				value = rand(min,max);
@@ -262,12 +299,14 @@ function generateOperations(_qtt, _lvl) {
 		return value
 	};
 
-	console.table(level);
+
+  console.log(level);
+	console.log(response);
 	return level;	
 };
 
 // module.exports = generateOperations;
 
-for(let i = 0; i < 10; i++) {
-  generateOperations(10,i);
-};
+// for(let i = 0; i < 10; i++) {
+  generateOperations(10,9);
+// };
