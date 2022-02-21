f68ad55ba3115dd131afb6952f39c565dadc6bab
ayoline <ayo@outlook.com.br>
2022-02-21 13:24:30 -0300

    update generateProblems module with some new features

--- backend/modules/generateProblems.js
+++ backend/modules/generateProblems.js
@@ -14,11 +14,30 @@ function generateProblemsByLevel(_lvl) {
 
     randomProblems = shuffleLvlProblems(randomProblems);
 
+    // To separate the problem and de response of the problem in 2 arrays
     randomProblems = randomProblems.map(item => {
         const split = item.replace(/\s/g, '').split('=');
         const arr = split[0].replace(/[+-/x]/g, 'SPACE').split('SPACE');
-        return [[...arr, '01', '02', '03', '04', '05', '06'], [split[1]]];
+        return [[...arr], [split[1]]];
     });
+
+    // To generate random numbers until get 10 positions in array
+    for (let i = 0; i < randomProblems.length; i++) {
+        for (let j = randomProblems[i][0].length; j < 10; j++) {
+            do {
+                add = true;
+                let randomTempNumber = getRandomInt(1, 99);
+                if (randomProblems[i][0].includes(randomTempNumber)) {
+                    add = false;
+                } else {
+                    randomProblems[i][0].push(randomTempNumber.toString());
+                }
+            } while (add == false);
+        }
+        randomProblems[i][0] = shuffleLvlProblems(randomProblems[i][0]);
+    }
+
+    removeLowerPositions(randomProblems);
 
     return randomProblems;
 }
@@ -34,6 +53,18 @@ function shuffleLvlProblems(_array) {
             _array[randomIndex], _array[currentIndex]];
     }
     return _array;
+}
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.ceil(Math.random() * (max - min + 1));
+}
+
+function removeLowerPositions(_array) {
+    while (_array.length > 3) {
+        _array.pop();
+    }
 }
 
 module.exports = generateProblemsByLevel;
