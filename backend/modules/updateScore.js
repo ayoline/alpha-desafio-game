89137c898c167b6da18ff0862d0e0522cab5162d
ayoline <ayo@outlook.com.br>
2022-02-21 21:05:09 -0300

    update env and updateScore with some corrections

--- backend/modules/updateScore.js
+++ backend/modules/updateScore.js
@@ -1,8 +1,9 @@
 
 // the parameters format needed: startDate = new Date(); endDate = new Date(); lvl = intNumber;
 function updateScore(_startDate, _endDate, _lvl) {
+    const maxTime = process.env.ROUND_TIME;
     const timeExpendInSeconds = (_endDate.getTime() - _startDate.getTime()) / 1000;
-    const timeLeft = _maxTime - parseInt(timeExpendInSeconds);
+    const timeLeft = parseInt(maxTime) - parseInt(timeExpendInSeconds);
     let scoreCalculated;
     if (_lvl === 1) { scoreCalculated = timeLeft * 1 };
     if (_lvl === 2) { scoreCalculated = timeLeft * 2 };
